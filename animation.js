e11ab7575e420870bda9bc2a226479159e7483e0
ctrlxwq <veronikashoha602@yahoo.com>
2023-05-14 13:34:13 +0300

    first commit

---
+++ animation.js
@@ -0,0 +1,27 @@
+// Lottie animation
+let animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'data.json' // шлях до вашого JSON-файлу
+  });
+
+//Button animation
+function colorChange1(selector, colors, ms){
+    var el=document.querySelector(selector);
+    var anime=[];
+    for(i=0;i<colors.length;i++)
+        anime[i]={borderColor:colors[i]};
+    el.animate(anime,{duration:ms, iterations:Infinity});
+}
+colorChange1('.btn_contact',['#FFF500','#05f501','#00F0FF','#1400FF', '#FFF500'],10000);
+
+function colorChange2(selector, colors, ms){
+    var el=document.querySelector(selector);
+    var anime=[];
+    for(i=0;i<colors.length;i++)
+        anime[i]={color:colors[i]};
+    el.animate(anime,{duration:ms, iterations:Infinity});
+}
+colorChange2('.btn_contact',['#FFF500','#05f501','#00F0FF','#1400FF', '#FFF500'],10000);
---
+++ index.js
@@ -0,0 +1,8 @@
+
+let animation = lottie.loadAnimation({
+    container: document.getElementById('lottie-container'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'https://assets7.lottiefiles.com/packages/lf20_0zmxzr7c.json' // шлях до вашого JSON-файлу
+});
