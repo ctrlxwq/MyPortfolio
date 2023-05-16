c3e2fa68b5acc56b5b74cc88abd23683199c5b75
ctrlxwq <veronikashoha602@yahoo.com>
2023-05-16 23:51:59 +0300

    third commit

--- animation.js
+++ animation.js
@@ -1,27 +1,19 @@
-// Lottie animation
-let animation = lottie.loadAnimation({
-    container: document.getElementById('lottie-container'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: 'data.json' // шлях до вашого JSON-файлу
-  });
 
 //Button animation
-function colorChange1(selector, colors, ms){
+function colorChangeBorder(selector, colors, ms){
     let el=document.querySelector(selector);
     let anime=[];
-    for(i=0;i<colors.length;i++)
+    for( i=0;i<colors.length;i++)
         anime[i]={borderColor:colors[i]};
     el.animate(anime,{duration:ms, iterations:Infinity});
 }
-colorChange1('.btn_contact',['#FFF500','#05f501','#00F0FF','#1400FF', '#FFF500'],10000);
+colorChangeBorder('.btn_contact',['#05f501','#00F0FF','#1400FF'],10000);
 
-function colorChange2(selector, colors, ms){
+function colorChangeText(selector, colors, ms){
     let el=document.querySelector(selector);
     let anime=[];
     for(i=0;i<colors.length;i++)
         anime[i]={color:colors[i]};
     el.animate(anime,{duration:ms, iterations:Infinity});
 }
-colorChange2('.btn_contact',['#FFF500','#05f501','#00F0FF','#1400FF', '#FFF500'],10000);
+colorChangeText('.btn_contact',['#05f501','#00F0FF','#1400FF'],10000);
